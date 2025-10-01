6e7b772a78df55f68a78cdbf84b1e73b24b7c453
hilmimahdani <hilmimahdanib@gmail.com>
2025-10-01 17:04:51 +0700

    tambah fitur login dan logout by Hilmi_Mahdani_Bilda

--- app/Views/anggota/create.php
+++ app/Views/anggota/create.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Mahasiswa</title>
+    <title>Tambah Anggota DPR</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-5">
     <div class="card shadow-lg">
         <div class="card-header bg-success text-white">
-            <h3>➕ Tambah Mahasiswa</h3>
+            <h3>➕ Tambah Anggota DPR</h3>
         </div>
         <div class="card-body">
             <form method="post" action="<?= base_url('anggota/store') ?>">
